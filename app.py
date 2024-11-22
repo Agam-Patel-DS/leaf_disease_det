3b7a9c3b870228aeedea1c76ba52539ec6e8c7cf
Agam-Patel-DS <colabwithagam@gmail.com>
2024-11-22 09:43:53 +0000

    Final Check Done

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ import os
 import yaml
 import streamlit as st
 
+
+st.title("Leaf Disease Detection")
 from src.prediction import prediction_config, predict
 pred_params=yaml.safe_load(open("params.yaml"))["prediction"]
 
 config=prediction_config(pred_params)
 
 pred=predict(config)
-pred.predict_image("/content/drive/MyDrive/leaf_disease_detection/sample_images/potato.jpeg")
+
+image=st.file_uploader("Upload your image!")
+if st.button("Predict"):
+  disease=pred.predict_image(image)
+  st.write(f"The disease is {disease}")
